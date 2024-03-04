02a175042d8e671ffcb40cd38fba4fff6ef10a14
Tim Almamedov <tim.almamedov@gmail.com>
2024-03-04 09:44:45 +0100

    fix

--- packages/bot-client/commands/commands_handler.ts
+++ packages/bot-client/commands/commands_handler.ts
@@ -6,7 +6,7 @@ import { startCommand } from "./start";
 import { process_shop_cmd } from "./select_shop_index_cmd";
 import { process_all_shops } from "./process_all_shops";
 import { quick_action } from "./quick_action";
-import { getQrCodesForAllShops } from "./get_qr_codes_for_all_shops";
+import { getQrCodesForAllShops } from "./get_previous_qr_codes_for_all_shops";
 
 export const attachCommands = (
   bot: Telegraf<MyContext<Update>>,
