2b1a20323e5e7442a3b2b31aa9b9cc20362a1bba
Tim Almamedov <tim.almamedov@gmail.com>
2024-12-24 11:23:16 +0100

    test

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,23 +1,26 @@
 module.exports = {
-    "apps": [
-        {
-            "name": "bot-client",
-            "cwd": "./packages/bot-client",
-            "script": "index.ts",
-            "interpreter": "bun",
-            "exp_backoff_restart_delay": 100,
-            "time": true,
-            "log_date_format": "YYYY-MM-DD HH:mm Z"
-        },
-        {
-            "name": "bot-backend",
-            "cwd": "./packages/backend",
-            "script": "./src/index.tsx",
-            "interpreter": "bun",
-            "exp_backoff_restart_delay": 150,
-            "cron_restart": '0 5 * * *',
-            "time": true,
-            "log_date_format": "YYYY-MM-DD HH:mm Z"
-        }
-    ]
-}
+  apps: [
+    {
+      name: "bot-client",
+      cwd: "./packages/bot-client",
+      script: "index.ts",
+      interpreter: "bun",
+      exp_backoff_restart_delay: 100,
+      time: true,
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+    },
+    {
+      name: "bot-backend",
+      cwd: "./packages/backend",
+      script: "./src/index.tsx",
+      interpreter: "bun",
+      exp_backoff_restart_delay: 150,
+      cron_restart: "0 5 * * *",
+      time: true,
+      log_date_format: "YYYY-MM-DD HH:mm Z",
+      env: {
+        NODE_OPTIONS: "--max-old-space-size=3072",
+      },
+    },
+  ],
+};
